d38834ad2d30450549deac0ba8526aa0b9bd5968
Anna-Bracha1 <33527752+Anna-Bracha1@users.noreply.github.com>
2017-11-11 16:41:26 +0100

    Update Snake_main.ino

--- Snake_main.ino
+++ Snake_main.ino
@@ -1,4 +1,4 @@
-//#include <string>
+///#include <string>
 #include <Arduino.h>
 #include <U8g2lib.h>
 #include <SPI.h>
@@ -10,19 +10,43 @@
 //using namespace std;
 
 U8G2_ST7920_128X64_2_SW_SPI game(U8G2_R2 /*rotate 180 degree*/, 13 /*clock*/, 11 /*data*/, 10 /*CS*/, 8 /*RST*/);
-Display display1 = Display(6, 6, 1, 1, 15, 7, 55, 33);
+Display display1 = Display(8, 8, 1, 1, 25, 7, 55, 33);
+int d=0;
 
 void setup() {
   game.begin(U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE);
-  Serial.begin(9600);
+  //Serial.begin(9600);
 }
 
 void loop() {
-  display1.setBody(1, RIGHT);
-  display1.setBody(2, RIGHT);
   game.firstPage();
   do {
     game.drawXBM(mapX, mapY, mapW, mapH, display1.bitMap);
   } while (game.nextPage());
-  delay(100);
+  delay(300);
+  if(d == 0)
+  {
+    display1.setBody(1, UP);
+    display1.setBody(2, UP);
+    //display1.setBody(2, RIGHT);
+    d=1;
+  }
+  else if(d == 1)
+  {
+    display1.setBody(1, UP);
+    display1.setBody(2, UP);
+    d=2;
+  }
+  else if(d == 2)
+  {
+    display1.setBody(1, RIGHT);
+    display1.setBody(2, RIGHT);
+    d=3;
+  }
+  else if(d == 3)
+  {
+    display1.setBody(1, RIGHT);
+    display1.setBody(2, RIGHT);
+    d=0;
+  }
 }
