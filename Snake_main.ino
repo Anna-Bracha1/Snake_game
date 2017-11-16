269690c22bd3bf9776228eba2fb711583d08038b
Anna-Bracha1 <33527752+Anna-Bracha1@users.noreply.github.com>
2017-11-16 19:36:09 +0100

    Update Snake_main.ino

--- Snake_main.ino
+++ Snake_main.ino
@@ -4,49 +4,40 @@
 #include <SPI.h>
 #include <Wire.h>
 #include "Display.h"
+#include "TimerOne.h"
 #define mapX 0 // X-position (left position of the bitmap)
 #define mapY 0 // Y-position (upper position of the bitmap)
 
 //using namespace std;
 
 U8G2_ST7920_128X64_2_SW_SPI game(U8G2_R2 /*rotate 180 degree*/, 13 /*clock*/, 11 /*data*/, 10 /*CS*/, 8 /*RST*/);
-Display display1 = Display(8, 8, 1, 1, 25, 7, 55, 33);
-int d=0;
+Display display1 = Display(8, 8, 1, 1, 25, 100, 55, 33);
+unsigned char d=0;
+DIRECTIONS directionH = UP;
 
 void setup() {
   game.begin(U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE, U8X8_PIN_NONE);
-  //Serial.begin(9600);
+  randomSeed(analogRead(0));
+  Timer1.initialize(5000000);
+  Timer1.attachInterrupt(timerISR);
+  pinMode(4, INPUT);  // set pin to input
+  //digitalWrite(4, HIGH);  // turn on pullup resistors
 }
 
-void loop() {
+void timerISR()
+{
+  display1.setBody(display1.player1, directionH);
+  display1.setBody(display1.player1, directionH);
+  display1.setBody(display1.player2, directionH);
+  display1.setBody(display1.player2, directionH);
   game.firstPage();
   do {
     game.drawXBM(mapX, mapY, mapW, mapH, display1.bitMap);
   } while (game.nextPage());
-  delay(300);
-  if(d == 0)
-  {
-    display1.setBody(1, UP);
-    display1.setBody(2, UP);
-    //display1.setBody(2, RIGHT);
-    d=1;
-  }
-  else if(d == 1)
-  {
-    display1.setBody(1, UP);
-    display1.setBody(2, UP);
-    d=2;
-  }
-  else if(d == 2)
-  {
-    display1.setBody(1, RIGHT);
-    display1.setBody(2, RIGHT);
-    d=3;
-  }
-  else if(d == 3)
-  {
-    display1.setBody(1, RIGHT);
-    display1.setBody(2, RIGHT);
-    d=0;
-  }
+  directionH = RIGHT;
+}
+
+void loop() {
+  //if(digitalRead(4) == HIGH)
+    directionH = UP;
 }
